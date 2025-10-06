c0bbf1d9b503722717a3eb2c4428ba3ba7616792
Praveen Reddy Goli <163567436+Praveen8104@users.noreply.github.com>
2025-10-06 20:39:20 +0530

    Fix loop condition in twoSum method

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] p=new int[2];
-       for(int i=0;i<nums.length-1;i++)
+       for(int i=0;i<=nums.length-2;i++)
        {
            for(int j=i+1;j<nums.length;j++)
            {
